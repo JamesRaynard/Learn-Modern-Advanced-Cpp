762837de602dfd66031c845d7111c342f78f606d
sharonh <sharonh102@gmail.com>
2024-02-28 09:36:08 +0200

    minor

--- copy_constructor_overview/string.cc
+++ copy_constructor_overview/string.cc
@@ -8,20 +8,38 @@ class String {
 	string s;
 public:
 	// Constructors
+    String() : s("") {}
 	String(const string& s) : s(s) {}
 	
 	// Copy constructor
-	String(const String& arg) : s(arg.s) {}
+	String(const String& arg) : s(arg.s) {cout << "copy constructor " << s << " called" << endl;}
+
+    // Copy assignment
+    String& operator=(const String& other) {
+        if (this == &other) {
+            return *this; // Return the current object
+        }
+
+        // Copy data from the source object
+        s = other.s;
+        cout << "copy assignment " << s << " called" << endl;
+        // Return the current object to allow chaining assignments
+        return *this;
+    }
 	
 	void print() { cout << s << endl; }
 };
 
 int main() {
-	String w { "world"s };   // Call constructor
-	String bang { w };       // Call copy constructor
+	String h { "hello" };   // Call constructor
+    String w { "world" };   // Call constructor
+    String g { "what's up" };   // Call constructor
+	String bang { h };       // Call copy constructor
 	String bang2 = w;        // Call copy constructor
-	
-	cout << "w = "; w.print();
+	String bang3;            // Call copy assignment
+	bang3 = g;
+
 	cout << "bang = "; bang.print();
 	cout << "bang2 = "; bang2.print();
+	cout << "bang3 = "; bang3.print();
 }
